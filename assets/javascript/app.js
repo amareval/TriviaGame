4e13ccb0c7014c14dda0cbf9bd9bb15608f7c21b
alexarevalo <aarevalo@snap.com>
2018-03-22 21:55:53 -0700

    got the timer to start counting down. Will work towards populating questions and then the multiple choice.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,92 +1,53 @@
-// Want to have playey number equal a function of adding all the morty images being clicked
+//define the timer and variables that will remain constant. Question, timer, and multiple choice
 
-var playerNumber = 0;
+var timer = 31;
+var intervalId;
 
-//Run when page loads
+var questions = {
 
-$(document).ready(function(){
+    question1: "Wanna know how I got these scars?",
+    question2: "King Kong aint got shit on me!",
+    question3: "Are you not entertained!",
+    question4: "My precious"
 
-//Add the score counter into the "score" div
-var wins = 0;
-var losses = 0;
-
-
-
-//A function to start the game and eventually call back when we need to reset
-
-function startGame(){
-//Add that number into the number to match
- computerNumber = Math.floor(Math.random() * 120 + 19);
-$(".computerNumber").html(computerNumber);
-
-$(".score").html("Wins: " + wins + "<br>" + "Losses: " + losses);
-playerNumber = 0;
 };
-//Function to start the game with the above code
-startGame();
-
-// Next step is to define the buttons as random number generators at the beginning of the game
-
-function assignNumbers(){
- rick1 = Math.floor(Math.random() * 12 + 1);
- rick2 = Math.floor(Math.random() * 12 + 1);
- rick3 = Math.floor(Math.random() * 12 + 1);
- rick4 = Math.floor(Math.random() * 12 + 1);
-
-return [rick1 , rick2, rick3, rick4];
-};
-//Assign function value to variable
- var rickNumbers = assignNumbers();
-
-console.log(rickNumbers);
-
-//Now we add to the playerNumber amount when we click on an image.
-
-$('.rick1').on('click', function() {
-    playerNumber = (playerNumber + rickNumbers[0]);
-    console.log(playerNumber);
-    console.log(rickNumbers[0])
-})
-$('.rick2').on('click', function() {
-    playerNumber = (playerNumber + rickNumbers[1]);
-})
-$('.rick3').on('click', function() {
-    playerNumber = (playerNumber + rickNumbers[2]);
-})
-$('.rick4').on('click', function() {
-    playerNumber = (playerNumber + rickNumbers[3]);
-})
-
-// Inputting the player number into the div
-
-$('.pic').on('click', function(){
-$('.yourNumber').text(playerNumber);
-});
-
-//Now want to compare with if statements when the player loses or wins. Also add a reset button
 
 
+var multipleC;
 
-$('.rick').on('click', function(){
+decrement()
+
+
+function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+//Set the timer to start decrementing (Function 1)
+function decrement() {
+
+    timer--;
+
+    $("#show-timer").addClass("text-center btn-warning w-100");
+    $("#show-timer").html("<h2>" + timer + "</h2>");
     
-if (playerNumber == computerNumber){
-    wins++
-    startGame();
-    assignNumbers();
-    rickNumbers = assignNumbers();
-    console.log(rickNumbers);
-}
-else if (playerNumber > computerNumber){
-    losses++
-    startGame();
-    assignNumbers();
-    rickNumbers = assignNumbers();
-    console.log(rickNumbers);
-}
-});
 
-});
+    };
+
+    run();
 
 
 
-//Add all numbers in an array, possible?
+//Show the question (function 2)
+
+//Show the multiple choice answers for that question (Function 3)
+
+//If the answer is correct, show the gif and say correct
+
+//if the answer is incorrect, show the gif and say incorrect
+
+//If the timer hits 0 then show "out of time", show gif
+
+//Once all the above run, then restart automatically after 5 seconds
+
+//Add the next question, add the next multiple choice set
